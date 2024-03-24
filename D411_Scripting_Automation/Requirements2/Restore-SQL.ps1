32edc5f673a2b12b027b8bf28504e31457e334a2
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 14:10:06 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
@@ -3,13 +3,12 @@
 $dbType = "Microsoft.SqlServer.Management.Smo.Server"
 $dbName = "ClientDB"
 $sqlInstanceName = "SRV19-PRIMARY\SQLEXPRESS"
-$count = 1
 try {
     # Remove old sql module due to function conflicts.
     if (Get-Module -Name sqlps) { Remove-Module sqlps }
     # install new sql module.
     Import-Module -Name SqlServer
-
+    
     # Create SQL database
     $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
     $dbObject.Create()
@@ -17,8 +16,14 @@ try {
     Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
     # Get Customer_Leads data from csv
     $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
-
+    $AllUsers = $customer_leads.count
+    $count = 1
+    
     ForEach ($u in $customer_leads) {
+        # Show progress indeicator.
+        $progress = "[SQL]: Adding new SQL user $($Name). $($count) of $($AllUsers)"
+        Write-Progress -Activity "D411 Restore SQL DB" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
+
         $First = $u.first_name
         $Last = $u.last_name
         $Name = "$($First) $($Last)"
