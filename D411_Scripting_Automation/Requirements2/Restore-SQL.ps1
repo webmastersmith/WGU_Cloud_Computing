e71e0277d298bb01ecffe5eb9d078d34d7a39004
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 14:07:48 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
@@ -3,7 +3,7 @@
 $dbType = "Microsoft.SqlServer.Management.Smo.Server"
 $dbName = "ClientDB"
 $sqlInstanceName = "SRV19-PRIMARY\SQLEXPRESS"
-
+$count = 1
 try {
     # Remove old sql module due to function conflicts.
     if (Get-Module -Name sqlps) { Remove-Module sqlps }
@@ -13,7 +13,25 @@ try {
     # Create SQL database
     $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
     $dbObject.Create()
+    # Create the table from SQL file
+    Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
+    # Get Customer_Leads data from csv
+    $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
 
+    ForEach ($u in $customer_leads) {
+        $First = $u.first_name
+        $Last = $u.last_name
+        $Name = "$($First) $($Last)"
+        $City = $u.city
+        $County = $u.county
+        $Zip = $u.zip
+        $WorkPhone = $u.officePhone
+        $Mobile = $u.mobilePhone
+      
+        
+        $count++
+      }
+  
     
 }
 catch {
