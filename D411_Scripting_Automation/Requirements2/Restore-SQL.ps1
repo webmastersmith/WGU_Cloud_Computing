c95c063b9f187cedec2373eba502943ca8ec539c
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 13:52:17 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
@@ -1,7 +1,17 @@
 # Bryon Smith 011185815
 
+$dbName = "Microsoft.SqlServer.Management.Smo.Server"
+$sqlInstanceName = "SRV19-PRIMARY\SQLEXPRESS"
+
 try {
-    
+    # Remove old sql module due to function conflicts.
+    if (Get-Module -Name sqlps) { Remove-Module sqlps }
+    # install new sql module.
+    Import-Module -Name SqlServer
+
+    # Create SQL database
+    $my_db = New-Object -TypeName $dbName -ArgumentList
+
     
 }
 catch {
