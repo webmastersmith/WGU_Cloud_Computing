1a852309d0496a640c75727e58e49d3c0c3103d8
webmastersmith <webmastersmith37@gmail.com>
2024-03-24 17:09:21 -0500

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
@@ -5,37 +5,37 @@ $dbName = "ClientDB"
 $sqlInstanceName = "SRV19-PRIMARY\SQLEXPRESS"
 $tableName = "CustomerLeads"
 try {
-    # Remove old sql module due to function conflicts.
-    if (Get-Module -Name sqlps) { Remove-Module sqlps }
-    # install new sql module.
-    Import-Module -Name SqlServer
+  # Remove old sql module due to function conflicts.
+  if (Get-Module -Name sqlps) { Remove-Module sqlps }
+  # install new sql module.
+  Import-Module -Name SqlServer
 
-    # If database exist, drop it.
-    if (Get-SqlDatabase -Name $dbName -ServerInstance $sqlInstanceName -ErrorAction SilentlyContinue) {
-        Write-Host "$($dbName) database exist!"
-        # Drop Database.
-        $dropQuery = "ALTER DATABASE $($dbName) SET SINGLE_USER WITH ROLLBACK IMMEDIATE; `
+  # If database exist, drop it.
+  if (Get-SqlDatabase -Name $dbName -ServerInstance $sqlInstanceName -ErrorAction SilentlyContinue) {
+    Write-Host "$($dbName) database exist!"
+    # Drop Database.
+    $dropQuery = "ALTER DATABASE $($dbName) SET SINGLE_USER WITH ROLLBACK IMMEDIATE; `
                       USE master; `
                       DROP DATABASE $($dbName)"
-        Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -Query $dropQuery
-        Write-Host "$($dbName) has been dropped!"
-    }
-    # Create SQL database
-    Write-Host "Creating Database $($dbName)"
-    $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
-    $dbObject.Create()
-    # Create the table from SQL file
-    Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
-    # Get Customer_Leads data from csv
-    $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
-    $AllUsers = $customer_leads.count
-    $count = 1
-    $Insert = "INSERT INTO [$($tableName)] (first_name, last_name, city, county, zip, officePhone, mobilePhone)"
-    ForEach ($u in $customer_leads) {
-        # Show progress indeicator.
-        $progress = "[SQL]: Adding new SQL user $($u.first_name) $($u.last_name). $($count) of $($AllUsers)"
-        Write-Progress -Activity "D411 Restore SQL DB" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
-        $Values = "VALUES ( `
+    Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -Query $dropQuery
+    Write-Host "$($dbName) has been dropped!"
+  }
+  # Create SQL database
+  Write-Host "Creating Database $($dbName)"
+  $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
+  $dbObject.Create()
+  # Create the table from SQL file
+  Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
+  # Get Customer_Leads data from csv
+  $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
+  $AllUsers = $customer_leads.count
+  $count = 1
+  $Insert = "INSERT INTO [$($tableName)] (first_name, last_name, city, county, zip, officePhone, mobilePhone)"
+  ForEach ($u in $customer_leads) {
+    # Show progress indeicator.
+    $progress = "[SQL]: Adding new SQL user $($u.first_name) $($u.last_name). $($count) of $($AllUsers)"
+    Write-Progress -Activity "D411 Restore SQL DB" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
+    $Values = "VALUES ( `
                 '$($u.first_name)', `
                 '$($u.last_name)', `
                 '$($u.city)', `
@@ -44,15 +44,16 @@ try {
                 '$($u.officePhone)', `
                 '$($u.mobilePhone)')"
         
-        # Add Insert statement to database.
-        $query = $Insert + $Values
-        Invoke-Sqlcmd -Database $dbName -ServerInstance $sqlInstanceName -Query $query
-        $count++
-    }
+    # Add Insert statement to database.
+    $query = $Insert + $Values
+    Invoke-Sqlcmd -Database $dbName -ServerInstance $sqlInstanceName -Query $query
+    $count++
+  }
 
-    Write-Host "$($dbName) database has been built with no errors!"    
+  Write-Host "$($dbName) database has been built with no errors!"
+  Invoke-Sqlcmd -Database $dbName –ServerInstance $sqlInstanceName -Query "SELECT * FROM dbo.Client_A_Contacts" > .\SqlResults.txt
 }
 catch {
-    Write-Host "Something went wrong!"
-    Write-Host $_
+  Write-Host "Something went wrong!"
+  Write-Host $_
 }
--- D411_Scripting_Automation/Scripting_Automation.md
+++ D411_Scripting_Automation/Scripting_Automation.md
@@ -5,3 +5,4 @@
 - <https://wgu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=a8427ab7-cd78-4d32-a043-adf10047b4d4>
 - <https://wgu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=724df24e-cfad-4271-9a7e-aff900d70a0e>
 - <https://www.reddit.com/r/WGU/comments/kljidt/c916_automation_scripting_completed_in_an_evening/>
+- <https://techcommunity.microsoft.com/t5/windows-powershell/sql-database-exists-in-powershell/m-p/3650376>
