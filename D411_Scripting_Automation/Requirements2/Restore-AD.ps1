269885b9d45092063816497024cb546982c70d6b
webmastersmith <webmastersmith37@gmail.com>
2024-03-24 14:26:43 -0500

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-AD.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-AD.ps1
@@ -12,29 +12,49 @@ try {
   # check if OU 'finance' exist
   Write-Host -ForegroundColor Blue "[AD]: Starting Active Directory Tasks"
   if (-Not([ADSI]::Exists("LDAP://$($ADPath)"))) {
-    Write-Host "$($OUCanonicalName) Does not exist."
-    # New-ADOrganizationalUnit -Name $OUCanonicalName -Path $ADRoot -DisplayName $OUDisplayName -ProtectedFromAccidentalDeletion $false
+    Write-Host "$($OUCanonicalName) Does not exist. Creating..."
+    New-ADOrganizationalUnit -Name $OUCanonicalName -Path $ADRoot -DisplayName $OUDisplayName -ProtectedFromAccidentalDeletion $false
+    Write-Host "$($OUCanonicalName) has been created."
   }
   else {
     Write-Host "$($OUCanonicalName) already exists."
   }
+  
+  
+  # Insert New Users in Active Directory from CSV file.
+  $AllUsers = $financeUsers.count
+  $count = 1  
+  ForEach ($fu in $financeUsers) {
+    $First = $fu.First_Name
+    $Last = $fu.Last_Name
+    $Name = "$($First) $($Last)"
+    $SamAcct = $fu.samAccount
+    $Zip = $fu.PostalCode
+    $WorkPhone = $fu.OfficePhone
+    $Mobile = $fu.MobilePhone
+  
+    # Show progress indeicator.
+    $progress = "[AD]: Adding AD user $($Name). $($count) of $($AllUsers)"
+    Write-Progress -Activity "D411 Restore AD" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
+    # Add User to AD  and show progress
+    New-ADUser -GivenName $First `
+      -Surname $Last `
+      -Name $Name `
+      -SamAccountName $SamAcct `
+      -DisplayName $Name `
+      -PostalCode $Zip `
+      -OfficePhone $WorkPhone `
+      -MobilePhone $Mobile `
+      -Path $ADPath
+    
+    # increment counter
+    count++
+  }
+
+  Write-Host "[AD] Restore Active Directory Task Complete."
+
 }
 catch {
   Write-Host "An error occurred:"
   Write-Host $_
-}
-
-ForEach ($fu in $financeUsers) {
-  $First = $fu.First_Name
-  $Last = $fu.Last_Name
-  $Name = "$($First) $($Last)"
-  $SamAcct = $fu.samAccount
-  $Zip = $fu.PostalCode
-  $WorkPhone = $fu.OfficePhone
-  $Mobile = $fu.MobilePhone
-
-  New-ADUser -GivenName $First `
-             -Surname $Last `
-             -Name $Name `
-             
 }
