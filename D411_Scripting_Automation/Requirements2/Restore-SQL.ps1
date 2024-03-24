7c674ecab4cb00050cb2c2bcaef9b1886f2b0977
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 14:54:27 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-SQL.ps1
@@ -20,22 +20,22 @@ try {
         Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -Query $dropQuery
         Write-Host "$($dbName) has been dropped!"
     }
-        # Create SQL database
-        Write-Host "Creating Database $($dbName)"
-        $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
-        $dbObject.Create()
-        # Create the table from SQL file
-        Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
-        # Get Customer_Leads data from csv
-        $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
-        $AllUsers = $customer_leads.count
-        $count = 1
-        $Insert = "INSERT INTO [$($tableName)] (first_name, last_name, city, county, zip, officePhone, mobilePhone)"
-        ForEach ($u in $customer_leads) {
-            # Show progress indeicator.
-            $progress = "[SQL]: Adding new SQL user $($Name). $($count) of $($AllUsers)"
-            Write-Progress -Activity "D411 Restore SQL DB" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
-            $values = "VALUES ( `
+    # Create SQL database
+    Write-Host "Creating Database $($dbName)"
+    $dbObject = New-Object -TypeName $dbType -ArgumentList $sqlInstanceName, $dbName
+    $dbObject.Create()
+    # Create the table from SQL file
+    Invoke-Sqlcmd -ServerInstance $sqlInstanceName -Database $dbName -InputFile $PSScriptRoot\CreateTable_CreateCustomerLeads.sql
+    # Get Customer_Leads data from csv
+    $customer_leads = Import-Csv $PSScriptRoot\NewClientData.csv
+    $AllUsers = $customer_leads.count
+    $count = 1
+    $Insert = "INSERT INTO [$($tableName)] (first_name, last_name, city, county, zip, officePhone, mobilePhone)"
+    ForEach ($u in $customer_leads) {
+        # Show progress indeicator.
+        $progress = "[SQL]: Adding new SQL user $($Name). $($count) of $($AllUsers)"
+        Write-Progress -Activity "D411 Restore SQL DB" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
+        $values = "VALUES ( `
                 '$($u.first_name)' `
                 '$($u.last_name)' `
                 '$($u.city)' `
@@ -43,8 +43,8 @@ try {
                 '$($u.zip)' `
                 '$($u.officePhone)' `
                 '$($u.mobilePhone)')"      
-            Invoke-Sqlcmd -Database $dbName -ServerInstance $sqlInstanceName -Query "$($Insert) $($values)"
-            $count++
+        Invoke-Sqlcmd -Database $dbName -ServerInstance $sqlInstanceName -Query "$($Insert) $($values)"
+        $count++
     }
     
 }
