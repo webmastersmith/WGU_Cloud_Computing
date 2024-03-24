cd6c145279c88fa28ad048170eba204be70e4d98
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 10:12:46 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-AD.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-AD.ps1
@@ -1,9 +1,16 @@
 # Bryon Smith 011185815
 
 $folderPath = $PSScriptRoot
-
+$ADRoot = "DC=consultingfirm,DC=com"
+$ADName = "Finance"
 try {
-  
+  # check if OU 'finance' exist
+  Write-Host -ForegroundColor Blue "[AD]: Starting Active Directory Tasks"
+  if (Get-ADOrganizationalUnit -Filter "distinguishedName -eq $ADName") {
+    Write-Host "OU Finance already exists."
+  } else {
+    New-ADOrganizationalUnit -Name $ADName -Path $ADRoot -DisplayName $ADName -ProtectedFromAccidentalDeletion $false
+  }
 }
 catch {
   Write-Host "An error occurred:"
