3959d63a4137f99968b9629bf299003e9b1f48d9
Lab Admin <LabAdmin@consultingfirm.com>
2024-03-24 12:41:50 -0700

    minor change

--- D411_Scripting_Automation/Requirements2/Restore-AD.ps1
+++ D411_Scripting_Automation/Requirements2/Restore-AD.ps1
@@ -12,46 +12,51 @@ try {
   # check if OU 'finance' exist
   Write-Host -ForegroundColor Blue "[AD]: Starting Active Directory Tasks"
   if (-Not([ADSI]::Exists("LDAP://$($ADPath)"))) {
+    # Create OU
     Write-Host "$($OUCanonicalName) Does not exist. Creating..."
     New-ADOrganizationalUnit -Name $OUCanonicalName -Path $ADRoot -DisplayName $OUDisplayName -ProtectedFromAccidentalDeletion $false
     Write-Host "$($OUCanonicalName) has been created."
+
+    # Insert New Users in Active Directory from CSV file.
+    $AllUsers = $financeUsers.count
+    $count = 1  
+    ForEach ($fu in $financeUsers) {
+      $First = $fu.First_Name
+      $Last = $fu.Last_Name
+      $Name = "$($First) $($Last)"
+      $SamAcct = $fu.samAccount
+      $Zip = $fu.PostalCode
+      $WorkPhone = $fu.OfficePhone
+      $Mobile = $fu.MobilePhone
+    
+      # Show progress indeicator.
+      $progress = "[AD]: Adding AD user $($Name). $($count) of $($AllUsers)"
+      Write-Progress -Activity "D411 Restore AD" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
+      # Add User to AD  and show progress
+      New-ADUser -GivenName $First `
+        -Surname $Last `
+        -Name $Name `
+        -SamAccountName $SamAcct `
+        -DisplayName $Name `
+        -PostalCode $Zip `
+        -OfficePhone $WorkPhone `
+        -MobilePhone $Mobile `
+        -Path $ADPath
+      
+      # increment counter
+      $count++
+    }
   }
   else {
-    Write-Host "$($OUCanonicalName) already exists."
+    Write-Host "$($OUCanonicalName) already exists. Removing..."
+    Remove-ADOrganizationalUnit -Identity $ADPath -Confirm:$false
+    Write-Host "$($OUCanonicalName) Removed. Please run script again."
+    Exit
   }
   
   
-  # Insert New Users in Active Directory from CSV file.
-  $AllUsers = $financeUsers.count
-  $count = 1  
-  ForEach ($fu in $financeUsers) {
-    $First = $fu.First_Name
-    $Last = $fu.Last_Name
-    $Name = "$($First) $($Last)"
-    $SamAcct = $fu.samAccount
-    $Zip = $fu.PostalCode
-    $WorkPhone = $fu.OfficePhone
-    $Mobile = $fu.MobilePhone
-  
-    # Show progress indeicator.
-    $progress = "[AD]: Adding AD user $($Name). $($count) of $($AllUsers)"
-    Write-Progress -Activity "D411 Restore AD" -Status $progress -PercentComplete (($count / $AllUsers) * 100)
-    # Add User to AD  and show progress
-    New-ADUser -GivenName $First `
-      -Surname $Last `
-      -Name $Name `
-      -SamAccountName $SamAcct `
-      -DisplayName $Name `
-      -PostalCode $Zip `
-      -OfficePhone $WorkPhone `
-      -MobilePhone $Mobile `
-      -Path $ADPath
-    
-    # increment counter
-    count++
-  }
 
-  Write-Host "[AD] Restore Active Directory Task Complete."
+  Write-Host "[AD] Restore Active Directory Task Completed with no errors."
 
 }
 catch {
